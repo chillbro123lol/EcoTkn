63e22868fd4283a4337e180de3f553e1f9f4268e
testuser123 <168487906+chillbro123lol@users.noreply.github.com>
2024-05-31 17:59:42 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    tokenBalance: { type: Number, default: 0 },
+    metaMaskAccount: { type: String },
+    submissions: [{
+        description: String,
+        proof: String,
+        status: { type: String, default: 'pending' }
+    }]
+}, { collection: 'ecotoken' });
 
 module.exports = mongoose.model('User', UserSchema);
